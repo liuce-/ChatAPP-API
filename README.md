283b62d2978f317660233e784b00422c3b598dfb
liuce- <lc453297223@qq.com>
2019-11-25 10:21:58 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChatAPP API
+
+Thank you for your support and trust! This is our chat app API. Feel free to ask questions on Piazza about our abstract classes and interfaces.
---
+++ src/main/java/edu/rice/comp504/model/User.java
@@ -0,0 +1,15 @@
+package edu.rice.comp504.model;
+
+import edu.rice.comp504.cmd.AbstractCmd;
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * A app user.
+ */
+public class User {
+}
